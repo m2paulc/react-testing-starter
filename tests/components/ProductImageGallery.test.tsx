0fee8dd187c0508b5e6afa239ac0dcdd3df949a9
Paul Caoile <39387011+m2paulc@users.noreply.github.com>
2024-04-30 09:26:15 -0400

    Test ProductImageGallery component

---
+++ tests/components/ProductImageGallery.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import ProductImageGallery from "../../src/components/ProductImageGallery";
+
+describe("ProductImageGallery", () => {
+	it("should render nothing if array is empty", () => {
+		const { container } = render(<ProductImageGallery imageUrls={[]} />);
+		expect(container).toBeEmptyDOMElement();
+	});
+
+	it("should render list of images if array is not empty", () => {
+		const imageUrls = [
+			"https://picsum.photos/id/100/200/300",
+			"https://picsum.photos/id/101/200/300",
+		];
+		render(<ProductImageGallery imageUrls={imageUrls} />);
+		const images = screen.getAllByRole("img");
+		expect(images).toHaveLength(2);
+		imageUrls.forEach((url, index) => {
+			expect(images[index]).toHaveAttribute("src", imageUrls[index]);
+		});
+	});
+});
