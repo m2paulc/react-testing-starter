4edd6ba61594958d1f4eb01af0f6f647dfd86db5
Paul Caoile <39387011+m2paulc@users.noreply.github.com>
2024-04-30 10:18:16 -0400

    test user interaction using userEvent on TermsAndConditions component

---
+++ tests/components/TermsAndConditions.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
+
+describe("TermsAndConditions", () => {
+	it("should render title and initial state", () => {
+		render(<TermsAndConditions />);
+		const heading = screen.getByRole("heading");
+		expect(heading).toBeInTheDocument();
+		expect(heading).toHaveTextContent(/terms & conditions/i);
+
+		const checkbox = screen.getByRole("checkbox");
+		expect(checkbox).toBeInTheDocument();
+		expect(checkbox).not.toBeChecked();
+
+		const button = screen.getByRole("button", { name: /submit/i });
+		expect(button).toBeDisabled();
+	});
+
+	it("should enable button when user checked the checkbox", async () => {
+		render(<TermsAndConditions />);
+		const checkbox = screen.getByRole("checkbox");
+		const user = userEvent.setup();
+		await user.click(checkbox);
+
+		const button = screen.getByRole("button", { name: /submit/i });
+		expect(button).toBeInTheDocument();
+		expect(button).toBeEnabled();
+	});
+});
